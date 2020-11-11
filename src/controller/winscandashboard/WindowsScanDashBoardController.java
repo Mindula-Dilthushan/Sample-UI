314a7a7db06a56ba204040a99661a853a815e0ce
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2020-11-11 13:08:47 +0530

    pc scan software ui

---
+++ src/controller/winscandashboard/WindowsScanDashBoardController.java
@@ -0,0 +1,4 @@
+package controller.winscandashboard;
+
+public class WindowsScanDashBoardController {
+}
---
+++ src/view/winscandashboard/DarkWinScanFormController.java
@@ -0,0 +1,4 @@
+package view.winscandashboard;
+
+public class DarkWinScanFormController {
+}
